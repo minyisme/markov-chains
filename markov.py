e4d0687cbcfb610ac98ca525a05947a820d35c41
Hackbright Student <info@hackbrightacademy.com>
2016-04-12 16:29:44 -0700

    completed up to part 2 of futher study

--- markov.py
+++ markov.py
@@ -1,5 +1,5 @@
 from random import choice
-
+import sys
 
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
@@ -15,7 +15,7 @@ def open_and_read_file(file_path):
     return file_text
 
 
-def make_chains(text_string):
+def make_chains(text_string, n_gram_length):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -32,26 +32,30 @@ def make_chains(text_string):
 
     # your code goes here
     text_list = text_string.split()
-    # Iterates through the list of words to create tuples (bi-grams)
-    # Those tuples become dictionary keys with a default value of []
-    for x in range(len(text_list)-1):
-        text_key = (text_list[x], text_list[x + 1])
+    text_key = []
+    # Iterates through the list of words to create lists of n_gram_length
+    for x in range(len(text_list) - n_gram_length + 1):
+        for i in range(n_gram_length):
+            text_key.append(text_list[x + i])
+        # Convert list to tuple to use as a dictionary key in chains
+        text_key = tuple(text_key)
         # If statement handles 1st entry of key-value and subsequent
         if text_key not in chains:
             try:
-                chains[text_key] = [text_list[x+2]]
+                chains[text_key] = [text_list[x + n_gram_length]]
             except IndexError:
                 chains[text_key] = [None]
         else:
             try:
-                chains[text_key].append(text_list[x+2])
+                chains[text_key].append(text_list[x + n_gram_length])
             except IndexError:
                 chains[text_key].append(None)
+        text_key = []
 
     return chains
 
 
-def make_text(chains):
+def make_text(chains, n_gram_length):
     """Takes dictionary of markov chains; returns random text."""
 
     text = ""
@@ -61,7 +65,10 @@ def make_text(chains):
 
     #Appends new word to words_list based using last 2 words of words_list as key
     while words_list[-1] != None:
-        tuple_to_look_up = (words_list[-2], words_list[-1])
+        list_to_look_up = []
+        for i in range(n_gram_length, 0, -1):
+            list_to_look_up.append(words_list[-i])
+        tuple_to_look_up = tuple(list_to_look_up)
         a_list_of_words = chains[tuple_to_look_up]
         new_word = choice(a_list_of_words)
         words_list.append(new_word)
@@ -72,16 +79,17 @@ def make_text(chains):
     return text
 
 
-input_path = "gettysburg.txt"
+input_path = sys.argv[1]
+n_gram_length = int(sys.argv[2])
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n_gram_length)
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, n_gram_length)
 
 print random_text
 
